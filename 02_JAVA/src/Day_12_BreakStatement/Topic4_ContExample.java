6405490ced1d21529d7116e595d6583ac7420c92
zyilmaz02 <zyylmz24@gmail.com>
2022-07-18 21:09:53 -0500

    Day 12 cont

---
+++ 02_JAVA/src/Day_12_BreakStatement/Topic4_ContExample.java
@@ -0,0 +1,28 @@
+package Day_12_BreakStatement;
+
+public class Topic4_ContExample {
+
+    public static void main(String[] args) {
+        // Write a code where it checks every chars in a string and when it sees a "L" or "A" pass
+        // print the end result
+
+        String ex = "HELLOAPPLE";
+        String result = "";
+
+        for (int i = 0; i < ex.length(); i++) {
+
+            //result = result + ex.charAt(i); // this will not serve your point
+
+            if(ex.charAt(i) == 'L' || ex.charAt(i) == 'A'){
+                continue;
+            }
+            result = result + ex.charAt(i);
+
+            // System.out.println(ex.charAt(i));
+
+
+        }
+        System.out.println(result);
+
+    }
+}
