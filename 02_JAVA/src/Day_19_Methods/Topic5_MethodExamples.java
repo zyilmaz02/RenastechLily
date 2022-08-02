1ad4e0bc56e8ba7c05be2a77b5d95c9f21e9ae73
zyilmaz02 <zyylmz24@gmail.com>
2022-08-01 21:06:57 -0500

    Day 19

---
+++ 02_JAVA/src/Day_19_Methods/Topic5_MethodExamples.java
@@ -0,0 +1,36 @@
+package Day_19_Methods;
+
+public class Topic5_MethodExamples {
+
+    // public               static           void               main        (String [] args){
+    // Access-modifer       specifier        return-type        methodName  (parameter)
+
+    public static void MyFirstMethod(){
+
+        System.out.println("This is my first method");
+
+    }
+
+    public static void SumtheNumbers(int a,int b){
+
+        int sum  = a+b;
+        System.out.println("the sum of those two numbers is: "+sum);
+
+        MyFirstMethod(); // Calling a method inside another method
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        MyFirstMethod();
+        System.out.println("This is inside of main method");
+       // MyFirstMethod();
+
+        SumtheNumbers(7,10);
+
+
+
+    }
+}
